b3acf634c7dc4b4e0a21b72425ec6ca45af6592f
Henrique <henrique.rodrigues7@hotmail.com>
2023-03-29 22:54:41 -0300

    update(home): add new components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,18 @@
 import Header from "../../components/header/Header";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import './home.css';
 
 export default function Home() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <div className="home-container">
+        <Sidebar/>
+        <Feed/>
+        <Rightbar/>
+      </div>
+    </>
   );
 }
