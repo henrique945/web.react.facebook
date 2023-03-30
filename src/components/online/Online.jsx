7a4968191354bc6bf2354cb5686cd225f0306b8e
Henrique <henrique.rodrigues7@hotmail.com>
2023-03-29 23:31:47 -0300

    feat(online): add Online component

---
+++ src/components/online/Online.jsx
@@ -0,0 +1,13 @@
+import './online.css';
+
+export default function Online({user}) {
+  return (
+    <li className="rightbar-friend">
+      <div className="rightbar-profile-img-container">
+        <img src={user.profilePicture} alt="" className="rightbar-profile-img"/>
+        <span className="rightbar-online"></span>
+      </div>
+      <span className="rightbar-username">{user.username}</span>
+    </li>
+  )
+}
