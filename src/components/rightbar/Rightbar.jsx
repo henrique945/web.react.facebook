35bda9323d1d3bc722fdc94b1cdd1cea10f270fb
Henrique <henrique.rodrigues7@hotmail.com>
2023-03-29 23:59:39 -0300

    update(rightbar): modified to be use with Home and Profile

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,26 +1,64 @@
 import './rightbar.css';
 import { Users } from "../../data";
 import Online from "../online/Online";
+import Following from "../following/Following";
 
-export default function Rightbar() {
+export default function Rightbar({profile}) {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthday-container">
+          <img src="/assets/gift.png" alt="" className="birthday-img"/>
+          <span className="birthday-text">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightbar-ad"/>
+        <h4 className="rightbar-title">Online Friends</h4>
+        <ul className="rightbar-friend-list">
+          {Users.map(user => {
+            if (user.id !== 1)
+              return <Online key={user.id} user={user}/>
+          })}
+        </ul>
+      </>
+    );
+  }
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbar-title">User information</h4>
+        <div className="rightbar-info">
+          <div className="rightbar-info-item">
+            <span className="rightbar-info-key">City:</span>
+            <span className="rightbar-info-value">New York</span>
+          </div>
+          <div className="rightbar-info-item">
+            <span className="rightbar-info-key">From:</span>
+            <span className="rightbar-info-value">Madrid</span>
+          </div>
+          <div className="rightbar-info-item">
+            <span className="rightbar-info-key">Relationship:</span>
+            <span className="rightbar-info-value">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbar-title">User friends</h4>
+        <div className="rightbar-followings">
+          {Users.map(user => {
+            if (user.id !== 1)
+              return <Following key={user.id} user={user}/>
+          })}
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
       <div className="rightbar">
         <div className="rightbar-wrapper">
-          <div className="birthday-container">
-            <img src="/assets/gift.png" alt="" className="birthday-img"/>
-            <span className="birthday-text">
-            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today.
-          </span>
-          </div>
-          <img src="/assets/ad.png" alt="" className="rightbar-ad"/>
-          <h4 className="rightbar-title">Online Friends</h4>
-          <ul className="rightbar-friend-list">
-            {Users.map(user => {
-              if (user.id !== 1)
-                return <Online key={user.id} user={user}/>
-            })}
-          </ul>
+          {profile ? <ProfileRightbar/> : <HomeRightbar/>}
         </div>
       </div>
     </>
